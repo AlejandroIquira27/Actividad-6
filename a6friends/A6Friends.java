2f237a8a55e26e90a9c2edcdc178aba6429f90b7
AlejandroIquira27 <126117636+AlejandroIquira27@users.noreply.github.com>
2023-06-29 10:58:56 -0500

    Add files via upload

---
+++ a6friends/A6Friends.java
@@ -0,0 +1,13 @@
+
+package a6friends;
+
+public class A6Friends {
+
+    public static void main(String[] args) {
+        
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
+        
+    }
+    
+}
